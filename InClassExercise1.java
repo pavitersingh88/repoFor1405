88c9312d84ad694a5f0e3c030ce93c29b35bd67a
pavitersingh88 <104886026+pavitersingh88@users.noreply.github.com>
2025-05-30 16:40:31 -0400

    Update InClassExercise1.java

--- InClassExercise1.java
+++ InClassExercise1.java
@@ -19,7 +19,7 @@ public class InClassExercise1
      */
     public static void main(String[] args)
     {
-        System.out.println("In Class Assignment 1 Components:");
+        System.out.println("In Class Assignment 1 Components  is : ");
         InClassExercise1.print1357();   // because print1357 is declared as static
         print1197531();
         print54321();
